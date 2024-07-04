0bace2bb800ab6a38fc14494f81790974e3b2ae2
vivek-bhave <bhavevivek7@gmail.com>
2024-07-03 20:45:25 -0700

    Add files via upload
    Initial commit of array operations.

---
+++ sumofArray.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+ int arr[5];
+ cout<<"Enter a number";
+ for(int i=0;i<5;i++){
+    cin>> arr[i];
+    
+ }   
+ cout << endl;
+ int sum = 0;
+ for( int i=0;i<=4;i++){
+    sum = sum + arr[i];
+ 
+} cout<< sum;}
