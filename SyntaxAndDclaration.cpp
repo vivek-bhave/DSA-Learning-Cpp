b114f66e63ba32ee6af77e45621e3b5c8efd565d
vivek-bhave <bhavevivek7@gmail.com>
2024-07-07 08:41:20 -0700

    Add files via upload
    Added additional array sorting algorithms

---
+++ SyntaxAndDclaration.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5]; // Declaration
+    arr[0] = 1; // initialisation
+    arr[1] = 2;
+    arr[2] = 3;
+ cout << arr[2]; // Accesing an array element
+
+}
