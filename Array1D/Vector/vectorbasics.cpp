572b35e3f7a7185223f7d4a751046f071e8a3c25
vivek-bhave <bhavevivek7@.in>
2024-07-23 07:20:14 -0700

    Updated Theory

--- Array1D/Vector/vectorbasics.cpp
+++ Array1D/Vector/vectorbasics.cpp
@@ -2,17 +2,18 @@
 #include<vector>
 using namespace std;
 int main(){
-    //capacity is the number of elements a vector can Hold.
-    //size the the number of elements in vector
+    //Capacity is the number of elements a vector can Hold.
+    //Size is the the number of elements in vector.
     vector <int> v; //Declaration (capacity: 0 , Size: 0)
-    v.push_back(4);//Insertion
-    v.push_back(3);//Pushback doubles vector's intial capacity/Size  (capacity: 1 , Size: 2)
-    v.push_back(4); // (capacity: 4 , Size: 3)
+    v.push_back(4);//Insertion  (capacity: 1 , Size: 1, Index: 0)
+    v.push_back(3);//Pushback doubles vector's intial capacity (capacity: 2 , Size: 2 ,Index: 1)
+    v.push_back(4); // (capacity: 4 , Size: 3, Index: 2)
+    cout<<"Intial value: "<< v[1] <<endl; //v[Index no.] = Output
     v[1] = 7; // Updation of first element
-    cout<< v[0]<<" ";//Output
-    cout<< v[1]<<" ";
-    cout<< v[2] <<" ";
-     cout<< v[3]<<" ";
+    cout<<"Updated Value: "<< v[1] <<endl;
+     //Output through loop
+     for(int i=0;i<=2;i++){
+     cout<<v[i]<<" ";}
 }
 
 
