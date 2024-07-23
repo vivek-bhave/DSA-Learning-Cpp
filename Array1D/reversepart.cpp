9da325fd8e2751725e66d0945d30372688606675
vivek-bhave <bhavevivek7@.in>
2024-07-23 08:32:01 -0700

    Added Reverse part Algorithm with detailed comments

--- Array1D/reversepart.cpp
+++ Array1D/reversepart.cpp
@@ -1,3 +1,10 @@
+//Q. WAP to reverse a part of vector/array.
+//Approach: 1.Initialise variables for vector size
+//2.Take Input vector and for Index parts i.e foe reversing a vector from index to index.
+//3.Creat function of reverse part which only revesrse the Index parts and pass  vector name and index numbers
+//such that those elements only get reversed inbetween index no.
+//4.Write swapping code in reverse part function.
+//5.Display a vector.
 #include<iostream>
 #include<vector>
 using namespace std;
@@ -8,7 +15,7 @@ void Display(vector<int>&v1){
     cout<< endl;
 }
 
-void reverse(int i,int j, vector<int>&v){
+void reversepart(int i,int j, vector<int>&v){
     
     int temp = 0;
     while(i<=j){
@@ -31,11 +38,11 @@ int main(){
         cin>> n;
         v.push_back(n);
     }
-    cout<<"Enter First index: ";
+    cout<<"Enter First index: "; // index parts
     cin>> a;
     cout<<"Enter second index: ";
     cin>> b;
    
-   reverse(a,b,v);
+   reversepart(a,b,v); 
    Display(v);
 }
