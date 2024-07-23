0955fbb88ade3cf3ea2dbb329701d0818151b293
vivek-bhave <bhavevivek7@.in>
2024-07-23 08:06:10 -0700

    Added reverse order Algorithm with detailed comments

--- Array1D/reverseorder.cpp
+++ Array1D/reverseorder.cpp
@@ -1,19 +1,25 @@
+//Q. WAP to reverse a array/Vector elements in a single Array.
+// Approach: 1. Take input first for size of array and then for vector elements through loop.
+// 2.Initialise variables for swapping vector elements
+//3.Write code for swapping vector elements in loop such that it swaps 1st with last. 2nd with 2nd last and so on until a condtion
+//4.Condition for loop i<=j cause for i<v.size() gives same vector.
 #include<iostream>
 #include<vector>
 using namespace std;
 int main(){
     int n,m;
+    cout<<"Enter the Size of vector: ";
     cin>> m;
-    cout<<"Enter a element: ";
+    cout<<"Enter the elements: ";
     vector<int> v;
     for(int i=0;i<m;i++){
         cin>> n;
         v.push_back(n);
-    }
+    } // intialisation for swapping variables
     int i=0,j=m-1,temp=0;
-    while(i!=m/2+1){
+    while(i<=j){ // condition for swapping
      temp = v[j];
-     v[j] = v[i];
+     v[j] = v[i]; // swapping code
      v[i] = temp;
      i++;
      j--;
