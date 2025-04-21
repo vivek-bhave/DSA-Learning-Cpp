eef47150f8c59784efa109c6c20021459c58b4fc
vivek-bhave <bhavevivek7@.in>
2025-04-21 00:48:26 -0700

    Added New Codes

---
+++ String/stringintialise.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+         // 1) string data type 
+//string ch = "Vivek Bhave";
+  //cout<< ch <<endl;
+        // 2) Through cin
+//string c;
+ // cin >> c; // Can Only take input without spaces
+ //cout << c <<endl; 
+        // 3)Through getline 
+string g;
+  getline(cin,g); // can take spaces
+  cout << g << endl;
+       
+}
